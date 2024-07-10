42daf9d40394a3bf4dbeca3e7bbd58e3f34ed0f6
Vitor Hugo Schwaab <vitor@schwaab.dev>
2024-07-10 14:36:12 +0200

    chore: sketch the exporter

--- backup/src/commonMain/kotlin/com/wire/backup/export/MPBackupExporter.kt
+++ backup/src/commonMain/kotlin/com/wire/backup/export/MPBackupExporter.kt
@@ -17,19 +17,47 @@
  */
 package com.wire.backup.export
 
+import com.oldguy.common.io.File
+import com.oldguy.common.io.FileMode
+import com.oldguy.common.io.ZipEntry
+import com.oldguy.common.io.ZipFile
 import com.wire.backup.data.BackupData
 
-class MPBackupExporter(private val exportPath: String) {
+class MPBackupExporter(exportPath: String) {
+
+    private val zipFile = ZipFile(
+        File(exportPath),
+        FileMode.Write
+    )
+
+    private val allUsers = ArrayList<BackupData.User>()
+    private val allConversations = ArrayList<BackupData.Conversation>()
+    private val allMessages = ArrayList<BackupData.Message>()
 
     fun add(user: BackupData.User) {
-
+        allUsers.add(user)
     }
 
     fun add(conversation: BackupData.Conversation) {
-
+        allConversations.add(conversation)
     }
 
     fun add(message: BackupData.Message) {
+        allMessages.add(message)
+    }
 
+    suspend fun flushToFile() {
+        // TODO: maybe perform BackupData -> storage format in parallel, instead of one entry at a time.
+        zipFile.use { file ->
+            file.addEntry(
+                ZipEntry("messages.json"),
+            ) {
+                // TODO: avoid parsing to String and then to ByteArray
+                //                    Maybe we can just output in another format like Protobuf
+                //                    And buffer it so we avoid having all of this in memory
+//                     KtxWebSerializer.json.encodeToString().encodeToByteArray()
+                TODO("Map all messages to a JSON and output as a ByteArray")
+            }
+        }
     }
 }
